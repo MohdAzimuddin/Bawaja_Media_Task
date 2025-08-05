86acc3ad1a4703087aec7c31a1e7fac1b9c53f68
MohdAzimuddin <sfsuper2020@gmail.com>
2025-08-05 19:08:36 +0530

    updated by removing unnecessary comment

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,13 @@
-// src/redux/store.js
 import { configureStore } from '@reduxjs/toolkit';
-import todoReducer from '../features/todo/todoSlice'; // Import your todo slice
+import todoReducer from '../features/todo/todoSlice'; 
 
-// Create the Redux store with your reducers
+// Create the Redux store with reducers
 export const store = configureStore({
   reducer: {
-    todos: todoReducer, // Add your todo reducer here
-    // You can add more reducers here as your app grows
+    todos: todoReducer, 
   },
   
   // Enable Redux DevTools in development
   devTools: process.env.NODE_ENV !== 'production',
 });
 
-// Optional: Export types for TypeScript or better autocompletion
-// export type RootState = ReturnType<typeof store.getState>;
-// export type AppDispatch = typeof store.dispatch;
