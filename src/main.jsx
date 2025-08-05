cb9060edc65dc4617a3209d3ef9a017c492690a8
MohdAzimuddin <sfsuper2020@gmail.com>
2025-08-05 18:25:47 +0530

    Add Import Provider Import Redux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'; // Import Provider
+import { store } from './redux/store'; // Import Redux store
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* Wrap*/}
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
